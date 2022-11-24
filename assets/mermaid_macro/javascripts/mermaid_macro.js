7e6eaa5835c142b614a8030d3567ee276ed2ebec
mitho-servicecenter <git@mitho-media.de>
2022-11-24 18:25:19 +0000

    update to 9.2.2 compatibility

--- assets/mermaid_macro/javascripts/mermaid_macro.js
+++ assets/mermaid_macro/javascripts/mermaid_macro.js
@@ -1,36 +1,27 @@
 // The conversion process when drawing the page is performed by initAllMermaidMacro().
 // After that, initMermaidMacro() is executed separately to support mermaid macro added by ajax, etc.
+(function ($) {
+  this.initMermaidMacro = initMermaidMacro;
 
-initAllMermaidMacro();
-
-function initAllMermaidMacro() {
-  if (isMermaidjsIsAvailable() === false) { return false; }
-
-  // Hide the target before the change by before-init-mermaid class until mermaid.initialize() or mermaid.init() is executed.
-  // After executing mermaid.initialize() or mermaid.init(), remove the before-init-mermaid class to show it.
-  mermaid.initialize({
-    startOnLoad: true,
-    mermaid: { callback:function() {
-      $('svg').parents('.mermaid.before-init-mermaid').removeClass('before-init-mermaid');
-    } }
+  $(function () {
+    mermaid.initialize({
+      startOnLoad: true
+    });
   });
-}
 
-function initMermaidMacro(id) {
-  if (isMermaidjsIsAvailable() === false) { return false; }
+  function initMermaidMacro(id) {
+    // If you run init on an element in the display: none state, the diagram will not be generated properly. In that case, do not run init.
+    // Example: This happens when you open the notes tab of the issue page directly.
+    const target = $('#' + id);
 
-  let target = $('#'+id+'.before-init-mermaid:not(:has(svg))');
-  // If you run init on an element in the display: none state, the diagram will not be generated properly. In that case, do not run init.
-  // Example: This happens when you open the notes tab of the issue page directly.
-  if (target && target.is(':visible')) {
-    mermaid.init(undefined, target);
+    if (target.is(':visible')) {
+      mermaid.init(undefined, target);
+    } else {
+      // Keep polling for whether the element becomes visible eventually, as it's relatively cheap.
+      setTimeout(function () {
+        initMermaidMacro(id);
+      }, 500);
+    }
   }
-}
+})(jQuery);
 
-function isMermaidjsIsAvailable() {
-  if (typeof mermaid === 'undefined') {
-    $('.mermaid').removeClass('before-init-mermaid');
-    return false;
-  }
-  return true;
-}
